e66c5a70f9365c479c2c2e56a119183d3842f977
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-07-29 08:32:07 -0700

    Add files via upload

---
+++ cylender_cost.py
@@ -0,0 +1,6 @@
+r = float(input("Enter your  radious : "))
+h = float(input("Enter your  height : "))
+v = 3.14*(r**2)*h
+print("your area  is : ",v)
+cost =v/1000*40
+print("your cost  is : ","Tk " ,cost)
